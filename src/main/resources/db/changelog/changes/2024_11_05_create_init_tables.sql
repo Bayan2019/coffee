dbc9ac331ef7b31a4856c12d363dbfb2b371e9f9
Bayan2019 <saparbayevabt@gmail.com>
2024-11-06 20:25:50 +0500

    first commit

---
+++ src/main/resources/db/changelog/changes/2024_11_05_create_init_tables.sql
@@ -0,0 +1,38 @@
+CREATE TABLE coffee(
+    id INT PRIMARY KEY,
+    name VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE ingridients(
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    quantity NUMERIC,
+    measure VARCHAR(255)
+);
+
+ALTER TABLE ingridients
+    ADD CONSTRAINT quantity0 CHECK (quantity>=0);
+
+CREATE TABLE recipes(
+    coffee_id INT REFERENCES coffee(id),
+    ingridient_id INT REFERENCES ingridients(id),
+    quantity NUMERIC
+);
+
+ALTER TABLE recipes
+    ADD CONSTRAINT quantity0 CHECK (quantity>0);
+
+CREATE TABLE orders(
+    id BIGINT PRIMARY KEY,
+    date TIMESTAMPTZ NOT NULL
+);
+
+ALTER TABLE orders
+    ADD CONSTRAINT date0817 CHECK (date::TIMESTAMPTZ::time >= '08:00:00' AND 
+        date::TIMESTAMPTZ::time <= '17:00:00');
+
+CREATE TABLE orders_coffee(
+    order_id BIGINT REFERENCES orders(id),
+    coffee_id INT REFERENCES coffee(id),
+    quantity INT
+);
